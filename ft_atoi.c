f002b7b844f0256c97f505d049f57d4d6f9e5b45
Zineb Ait-Errami <zait-err@c4r4p2.1337.ma>
2025-02-15 21:22:12 +0100

    update ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,39 +6,39 @@
 /*   By: zait-err <zait-err@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 13:42:34 by zait-err          #+#    #+#             */
-/*   Updated: 2025/02/15 21:10:59 by zait-err         ###   ########.fr       */
+/*   Updated: 2025/02/15 21:19:36 by zait-err         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_atoi(const char *str)
-{
-	int			i;
-	long long	resultat;
-	int			sign;
+// int	ft_atoi(const char *str)
+// {
+// 	int			i;
+// 	long long	resultat;
+// 	int			sign;
 
-	i = 0;
-	resultat = 0;
-	sign = 1;
-	if (!str)
-		return (0);
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -sign;
-		i++;
-	}
-	while (str[i] != '\0')
-	{
-		resultat = resultat * 10 + (str[i] - 48);
-		i++;
-	}
-	resultat = sign * resultat;
-	if (resultat < INT_MIN || resultat > INT_MAX)
-		print_error();
-	return (resultat);
-}
+// 	i = 0;
+// 	resultat = 0;
+// 	sign = 1;
+// 	if (!str)
+// 		return (0);
+// 	if (str[i] == '-' || str[i] == '+')
+// 	{
+// 		if (str[i] == '-')
+// 			sign = -sign;
+// 		i++;
+// 	}
+// 	while (str[i] != '\0')
+// 	{
+// 		resultat = resultat * 10 + (str[i] - 48);
+// 		i++;
+// 	}
+// 	resultat = sign * resultat;
+// 	if (resultat < INT_MIN || resultat > INT_MAX)
+// 		print_error();
+// 	return (resultat);
+// }
 
 // int mystrlen(char *str)
 // {
@@ -64,3 +64,49 @@ int	ft_atoi(const char *str)
 // 	else if(mystrlen(data[i]) == 11 && mystrlen(data[i]) == -2147483648)
 // 	else if(mystrlen(data[i]) == 10 && mystrlen(data[i]) == 2147483647)
 // }
+
+static int	is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_atoi(const char *str)
+{
+	int			i;
+	long long	result;
+	int			sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+
+	// Skip leading spaces
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+
+	// Handle sign
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+
+	// Check if next char is a digit after sign
+	if (!is_digit(str[i]))
+		print_error();
+
+	// Convert digits to integer
+	while (str[i] && is_digit(str[i]))
+	{
+		result = result * 10 + (str[i++] - '0');
+		if ((sign == 1 && result > INT_MAX) || (sign == -1 && -result < INT_MIN))
+			print_error();
+	}
+
+	// Check if trailing characters are invalid
+	if (str[i] != '\0')
+		print_error();
+
+	return ((int)(result * sign));
+}
